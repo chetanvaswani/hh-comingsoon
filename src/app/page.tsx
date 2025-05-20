dadde756660c6c86f96bbaab4532a592224f2a88
chetanvaswani <chetan.vaswani.cv@gmail.com>
2025-05-20 14:27:49 +0530

    added inline play for it to work on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+"use client"
 import React from "react";
 
 export default function Home() {
   return (
     <div className="h-svh w-full bg-black ">
-      <video loop autoPlay muted className="h-full w-full">
+      <video loop autoPlay muted playsInline className="h-full w-full">
           <source src="/comingsoon.MP4" type="video/mp4" />
           Your browser does not support the video.
     </video>
